8e462a1c08fd298d04406400c319d2c4697e7faf
hellorifat <52621094+hellorifat@users.noreply.github.com>
2024-04-01 23:38:05 +0600

    Add files via upload

---
+++ Lab 5.2/home.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+
+
+<html>
+    <body>
+        <h1></h1>
+            <table style="width:100%; border:0, align:center">
+                <tr style="height:150px">
+                    <td align="left"><a href="#"><img src="dflogo.png" alt="XCompany" width="80" height="80"></a></td>
+                    <td align="center"></td>
+                    <td align="right"><a href="#"> Home </a> | <a href="../login.php">Login</a> |  <a href="../registration.php">Registration</a><td>
+
+                </tr>
+                <tr style="height:600px" >
+                <td colspan="3"> Welcome to X Company</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="height:50px; text-align:center"> XCompany Copyright©2024</td>
+                </tr>
+        </table>
+    </body>
+</html>
