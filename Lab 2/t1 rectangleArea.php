bb197491f57bc6492f6d1f71a80fc75fabe0e9ae
hellorifat <52621094+hellorifat@users.noreply.github.com>
2024-04-01 13:10:55 +0600

    Add files via upload

---
+++ Lab 2/t1 rectangleArea.php
@@ -0,0 +1,13 @@
+<?php
+
+$rlength = 10;
+echo "Length: $rlength <br>";
+$rwidth = 20;
+echo "Width: $rwidth <br>";
+$rarea = $rlength * $rwidth;
+$rperimeter= 2*($rlength*$rwidth);
+
+echo "Area: $rarea <br>";
+echo "Perimeter: $rperimeter <br>";
+
+?>
