710270e434eab28c1a402b3716ef101b4389afa3
hellorifat <52621094+hellorifat@users.noreply.github.com>
2024-04-01 13:27:12 +0600

    Add files via upload

---
+++ Lab 2/t4_largestNumber.php
@@ -0,0 +1,24 @@
+<?php
+
+$num1 = 15;
+echo "Number 1 is : $num1 <br>";
+$num2 = 20;
+echo "Number 2 is : $num2 <br>";
+$num3 = 7;
+echo "Number 3 is : $num3 <br>";
+
+if ($num1>$num2 && num1>$num3)
+{
+echo "$num1 is the largest number<br>";
+}
+else if ($num2>$num1 && $num2>$num3) 
+{
+echo "$num2 is the largest number<br>";
+} 
+else 
+{
+echo "$num3 is the largest number<br>";
+} 
+
+
+?>
